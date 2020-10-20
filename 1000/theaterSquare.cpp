0a409681544d27db3cd89669aba3e12fcc72732b
Chris Sketch <sketchc89@gmail.com>
2020-10-19 23:42:30 -0400

    1000 questions

---
+++ 1000/theaterSquare.cpp
@@ -0,0 +1,11 @@
+#include <cmath>
+#include <cstdint>
+#include <iostream>
+
+int main() {
+  double l, w, a;
+  std::cin >> l >> w >> a;
+  int64_t res = std::ceil(l / a) * std::ceil(w / a);
+  std::cout << res;
+  return 0;
+}
