3067a642c954f69b574560f99f61e0ea7075ee89
Chris Sketch <sketchc89@gmail.com>
2020-10-20 22:48:24 -0400

    boredom game second attempt

---
+++ 1500/boredom.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  unordered_map<string, int> db;
+  ostringstream oss;
+  array<int64_t, 100001> counter, dp;
+
+  int n;
+  cin >> n;
+  vector<int> arr;
+  arr.reserve(n);
+
+  counter.fill(0);
+  copy_n(istream_iterator<int64_t>(cin), n, back_inserter(arr));
+  for (auto num : arr) {
+    counter[num]++;
+  }
+
+  if (counter.size() <= 1) {
+    cout << 0;
+    return 0;
+  }
+
+  dp[0] = 0;
+  dp[1] = 0;
+  for (int i = 2; i < counter.size(); ++i) {
+    dp[i] = max(dp[i - 1], dp[i - 2] + counter[i - 1] * (i - 1));
+  }
+
+  cout << dp[10000];
+  return 0;
+}
