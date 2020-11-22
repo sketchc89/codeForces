fdf735e2cb054be199d98ccc92e195ac39dfc140
Chris Sketch <sketchc89@gmail.com>
2020-11-21 22:09:06 -0500

    solve and comment

--- contests/677_3/district.cpp
+++ contests/677_3/district.cpp
@@ -4,32 +4,37 @@ using namespace std;
 
 string districtConnect(const vector<int>& gangs) {
     unordered_set<int> used;
+    /* If all equal then impossible to connect */
+    if (equal(gangs.cbegin() + 1, gangs.cend(), gangs.cbegin())) {
+        return "NO\n";
+    }
+    /* If not all equal, then connect the first district to every other district
+     * that isn't controlled by the first district gang,
+     * then connect the rest of the first district gang territory to any neighbor
+     */
     string res = "YES\n";
-    for (int i = 0; i < gangs.size(); ++i) {
-        if (used.count(i)) {
+    int connected = -1;
+    for (int i = 1; i < gangs.size(); ++i) {
+        if (gangs[0] == gangs[i]) {
             continue;
         }
-
-        bool foundRoad = false;
-        for (int j = i + 1; j < gangs.size(); ++j) {
-            if (gangs[i] == gangs[j]) {
-                continue;
-            }
-            // cout << i + 1 << ' ' << j + 1 << '\n';
-            foundRoad = true;
-            res += move(to_string(i + 1));
+        if (connected == -1) {
+            connected = i;
+        }
+        res.push_back('1');
+        res.push_back(' ');
+        res += move(to_string(i + 1));
+        res.push_back('\n');
+    }
+    for (int i = 1; i < gangs.size(); ++i) {
+        if (gangs[i] == gangs[0]) {
+            res += move(to_string(connected + 1));
             res.push_back(' ');
-            res += move(to_string(j + 1));
+            res += move(to_string(i + 1));
             res.push_back('\n');
-            used.insert(j);
-        }
-        if (foundRoad) {
-            used.insert(i);
         }
     }
-    if (used.size() != gangs.size()) {
-        res = "NO\n";
-    }
+
     return res;
 }
 
