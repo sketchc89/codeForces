7ccd875ecb45f4660f52c3339114c2169e75b077
Chris Sketch <sketchc89@gmail.com>
2020-12-28 18:10:57 -0500

    comments about ceiling

--- contests/101_edu/ceil.cpp
+++ contests/101_edu/ceil.cpp
@@ -11,6 +11,13 @@ int main() {
         cin >> N;
         vector<pair<int, int>> ops;
         int num = N;
+        /* Ceiling division x/y returns one if y > x
+         * If we eliminate all of the numbers after 2 then we are left with the final number
+         * The final number would take lg(N) steps if we just divided by 2
+         * sqrt shrinks faster than lg(N) so keep the sqrt rounded up around to divide N
+         * Keep the sqrt rounded up of that and so on.
+         * Every sqrt takes one extra step. The maximum number doable in 5 steps is 2^32, 6 steps 2^64...
+         * because we are continually raising to 1/2 power */
         while (num > 2) {
             int root = ceil(sqrt(num));
             for (int i = root + 1; i < num; ++i) {
